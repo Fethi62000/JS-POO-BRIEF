64c9db2b90fd513cc5f4e730dd670e3fc79a3c83
Fethisensei <fetam62@gmail.com>
2024-06-24 15:14:17 +0200

    feat(script-exo4.js): add class and function

---
+++ script-exo4.js
@@ -0,0 +1,17 @@
+class animal {
+    faireDuBruit(){
+        console.log("je fait du bruit.");
+    }
+}
+class Chien extends animal (){
+        faireDuBruitt(){
+            console.log("le chien aboie.");
+        }
+    }
+
+class animal extends Chat (){
+            fairreDuBruit(){
+                console.log("le chat miaule.");
+               }
+}
+
